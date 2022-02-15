bd19e63961861b63f862d0e193a7d038f4d24356
Kubaxoxo <kubakruger1@gmail.com>
2022-02-15 09:59:34 +0100

    python12-projekt1-madlibs

---
+++ madlibs.py
@@ -0,0 +1,6 @@
+adj = input("Przymiotnik: ")
+verb1 = input("Czasownik: ")
+verb2 = input("Czasownik: ")
+famous_person = input("Znana osoba: ")
+madlib = f"Programowanie komputerowe jest takie {adj}! To sprawia, że jestem cały czas podekscytowany, ponieważ \ Kocham to {verb1}. Pozostań nawodniony i {verb2} Jak ty jesteś {famous_person}!"
+print(madlib)
